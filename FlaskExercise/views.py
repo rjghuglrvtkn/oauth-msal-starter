48f80a677c213725743d07d7bfccbb96afa06525
rjghuglrvtkn <raama14@gmail.com>
2021-05-03 13:18:43 -0400

    modified views.py

--- FlaskExercise/views.py
+++ FlaskExercise/views.py
@@ -75,9 +75,16 @@ def _save_cache(cache):
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Create and return a Confidential Client Application from msal
-    return None
+    return msal.ConfidentialClientApplication(
+        Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
+        client_credential=Config.CLIENT_SECRET, token_cache=cache)
+    #return None
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Get the authorization request URL from a built msal app, and return it
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    #return None
